4a488c00dadfe2710e37b1ca62f275e2013e46a3
AyushRaviRai <ayushravirai@gmail.com>
2018-05-14 23:00:26 +0530

    commiting basic structure of node and learning stuff

---
+++ hello-world/app.js
@@ -0,0 +1,1 @@
+console.log("Helloo Mahaan World !! Tujhe kya ho gaya hain :P NM");
---
+++ notes-app/app.js
@@ -0,0 +1,4 @@
+console.log("Starting Our App Suckers get ready ");
+
+// requiring file system module
+const fs = require('fs');
