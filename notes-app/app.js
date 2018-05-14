da9c3a9d94f773de8df61f9ed85e0746aed06392
AyushRaviRai <ayushravirai@gmail.com>
2018-05-14 23:29:19 +0530

    adding fs module

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,3 +2,9 @@ console.log("Starting Our App Suckers get ready ");
 
 // requiring file system module
 const fs = require('fs');
+
+fs.appendFile("coolshit.txt", "We are gonna make some cool shit", function (error) {
+	if (error) {
+		console.log("File main nahi ghus paaya :P");
+	}	
+});
