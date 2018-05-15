1a77807f04d3dd96dd2cd18d79f79722b65a4240
AyushRaviRai <ayushravirai@gmail.com>
2018-05-15 22:54:22 +0530

    just clearing stuff for further operations

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,22 +3,11 @@ console.log(" == Starting app.js == ");
 // requiring file system module
 const fs = require('fs');
 
-// requring os module
-const os = require('os');
-user = os.userInfo();
-
 // requiring files
 const notes = require('./notes.js');
 
 // lodash module
 const _ = require('lodash');
 
-sum = notes.add(1,2);
-console.log("The Sum is  : " + sum);
-process.exit()
+console.log(process);
 
-fs.appendFile("coolshit.txt", `Hello This in from os module ${user.username}`, function (error) {
-	if (error) {
-		console.log("File main nahi ghus paaya :P");
-	}	
-});
