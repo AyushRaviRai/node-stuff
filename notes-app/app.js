84f7a60ea0e01b8ffe23547ce540d50c1e468f4e
Ayush Ravi <ayushravirai@gmail.com>
2018-05-15 18:33:03 +0530

    adding aaj ke changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,23 @@
-console.log("Starting Our App Suckers get ready hahaha");
+console.log(" == Starting app.js == ");
 
 // requiring file system module
 const fs = require('fs');
 
-fs.appendFile("coolshit.txt", "We are gonna make some cool shit", function (error) {
+// requring os module
+const os = require('os');
+user = os.userInfo();
+
+// requiring files
+const notes = require('./notes.js');
+
+// lodash module
+const _ = require('lodash');
+
+sum = notes.add(1,2);
+console.log("The Sum is  : " + sum);
+process.exit()
+
+fs.appendFile("coolshit.txt", `Hello This in from os module ${user.username}`, function (error) {
 	if (error) {
 		console.log("File main nahi ghus paaya :P");
 	}	
---
+++ notes-app/notes.js
@@ -0,0 +1,6 @@
+console.log(" == Starting notes.js ==");
+
+module.exports.add = (a, b) => {
+	console.log(" -- Add Method Called --");
+	return a + b
+}
