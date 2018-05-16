e9f856fe9649a7444af029e2856209adad94db0a
AyushRaviRai <ayushravirai@gmail.com>
2018-05-16 22:40:27 +0530

    experimenting with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,5 +9,23 @@ const notes = require('./notes.js');
 // lodash module
 const _ = require('lodash');
 
-console.log(process);
+// yargs
+const yargs = require('yargs');
 
+const argv = yargs.argv
+
+var action = argv.action;
+
+if (action === 'add') {
+	notes.addNote(argv.title, argv.body);
+} else if (action === 'list') {
+	notes.getAll();
+} else if (action === 'read') {
+	notes.readNote(argv.title);
+} else if (action === 'remove') {
+	notes.removeNote(argv.title);
+} else {
+	console.log("Bhai kuch to pass kar de , hawa main chalau kya !!");
+}
+
+ 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,6 +1,30 @@
 console.log(" == Starting notes.js ==");
 
-module.exports.add = (a, b) => {
-	console.log(" -- Add Method Called --");
-	return a + b
-}
+// Add Note 
+var addNote = (title, body) => {
+    console.log("Add Note Method : ", title, body);
+};
+
+// Getting All Notes
+var getAll = () => {
+    console.log("Getting All Notes");
+};
+
+// Note ko padh rahe hain 
+var readNote = (title) => {
+    console.log("Reading Note with title :", title);
+};
+
+// deleting node from our awsome
+var removeNote = (title) => {
+    console.log("Removing Note with title : ", title);
+};
+
+
+
+module.exports = {
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+}; 
